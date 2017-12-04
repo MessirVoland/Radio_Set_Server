a69034c029bc2bfbb45cb2f842bd5fcff59907ac
MessirVoland <messir-voland@mail.ru>
2017-12-04 13:01:20 +0300

    Initial commit

---
+++ android/src/ru/detone_studio/radio_set/server/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package ru.detone_studio.radio_set.server;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ru.detone_studio.radio_set.server.Main_Class;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Main_Class(), config);
+	}
+}
---
+++ desktop/src/ru/detone_studio/radio_set/server/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.detone_studio.radio_set.server.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.detone_studio.radio_set.server.Main_Class;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main_Class(), config);
+	}
+}
